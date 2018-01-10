afb8c0c72ca7f1c4fc52576dd47bed17b4db028d
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-10 22:05:21 +0000

    added a greeting with current date

--- lib/upcoming_games/cli.rb
+++ lib/upcoming_games/cli.rb
@@ -2,8 +2,15 @@
 class UpcomingGames::CLI
 
   def call
-    puts "Upcoming Games!"
+    puts "----------------------------"
+    greeting
+    puts "----------------------------"
   end
 
+  def greeting
+    date = Time.new
+    today = date.month.to_s + "/" + date.day.to_s + "/" + date.year.to_s
+    puts "Upcoming Games for #{today}"
+  end
 
 end
