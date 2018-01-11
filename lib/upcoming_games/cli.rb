1c73e4bf79143ab24cfd74a6fec588dceef47d50
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-11 16:27:39 +0000

    Done.

--- lib/upcoming_games/cli.rb
+++ lib/upcoming_games/cli.rb
@@ -25,9 +25,10 @@ class UpcomingGames::CLI
     input = gets.strip
     if input == "1"
       puts "Dragon Ball FighterZ"
-      puts "release date: January 26th, 2018"
-      puts "platform(s): XOne, PS4"
-      puts "genre: Fighting"
+      puts "Release date: January 26th, 2018"
+      puts "Platform(s): XOne, PS4"
+      puts "Genre: Fighting"
+      puts "Description: After the success of the Xenoverse series, its time to introduce a new classic 2D DRAGON BALL fighting game for this generations consoles. DRAGON BALL FighterZ is born from what makes the DRAGON BALL series so loved and famous: endless spectacular fights with its allpowerful fighters. Partnering with Arc System Works, DRAGON BALL FighterZ maximizes high end Anime graphics and brings easy to learn but difficult to master fighting gameplay to audiences worldwide. "
       puts "Want to look at more? Y/N?"
       response = gets.strip.upcase
       if response == "Y"
@@ -37,9 +38,10 @@ class UpcomingGames::CLI
       end
     elsif input == "2"
       puts "The Inpatient"
-      puts "release date: January 23rd, 2018"
-      puts "platform(s): PS4"
-      puts "genre: VR horror"
+      puts "Release date: January 23rd, 2018"
+      puts "Platform(s): PS4"
+      puts "Genre: VR horror"
+      puts "Description: Discover who you are before it’s too late in The Inpatient; a PS VR Exclusive set in the Blackwood Sanatorium, over 60 years prior to the 2016 BAFTA award-winning Until Dawn. With deep immersion that places YOU in the game, you take on the role of an amnesiac inpatient who must find out who you are and why you are in the Sanatorium. With a branching narrative, and different endings; every choice you make can drastically affect the way the game plays out."
       puts "Want to look at more? Y/N?"
       response = gets.strip.upcase
       if response == "Y"
