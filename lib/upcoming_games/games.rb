cb7e4431df5b958265326c55329b4eec11e04201
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-12 19:30:13 +0000

    removed unnecessary methods from CLI class

--- lib/upcoming_games/games.rb
+++ lib/upcoming_games/games.rb
@@ -29,22 +29,6 @@ class UpcomingGames::Games
     @description ||= game_description_doc.css("div.gameInfo p").text.strip.gsub("\r\n", "")
   end
 
-  def self.info
-    game_1 = self.new
-    game_1.name = "Dragon Ball FighterZ"
-    game_1.release_date = "January 26, 2018"
-    game_1.genre = "Fighting"
-    game_1.description = "After the success of the Xenoverse series, its time to introduce a new classic 2D DRAGON BALL fighting game for this generations consoles. DRAGON BALL FighterZ is born from what makes the DRAGON BALL series so loved and famous: endless spectacular fights with its allpowerful fighters. Partnering with Arc System Works, DRAGON BALL FighterZ maximizes high end Anime graphics and brings easy to learn but difficult to master fighting gameplay to audiences worldwide."
-    @@all << game_1
-    game_2 = self.new
-    game_2.name = "The Inpatient"
-    game_2.release_date = "January 23, 2018"
-    game_2.genre = "VR Horror"
-    game_2.description = "Discover who you are before it’s too late in The Inpatient; a PS VR Exclusive set in the Blackwood Sanatorium, over 60 years prior to the 2016 BAFTA award-winning Until Dawn. With deep immersion that places YOU in the game, you take on the role of an amnesiac inpatient who must find out who you are and why you are in the Sanatorium. With a branching narrative, and different endings; every choice you make can drastically affect the way the game plays out. "
-    @@all << game_2
-  end
-
-
     def self.scrape_upcoming_games
       doc = Nokogiri::HTML(open("http://www.ign.com/upcoming/games?platformSlug=ps4&sortBy=releaseDate&sortOrder=asc&time=3m"))
       games = doc.css("div.clear.itemList-item")
