5817be8117ca82148e9ce8bdb75cc510ef7bc205
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-12 17:54:02 +0000

    added new_from_page method and scraping methods

--- lib/upcoming_games/games.rb
+++ lib/upcoming_games/games.rb
@@ -9,13 +9,36 @@ class UpcomingGames::Games
     @release_date = release_date
     @genre = genre
     @url = url
+    @@all << self
   end
 
+  def self.new_from_page(g)
+    self.new(
+    g.css("h3 a").text.strip,
+    g.css("div.releaseDate.grid_3.omega").text.strip,
+    g.css("span.item-genre").text.strip,
+    g.css(".item-title h3 a").first.attr("href")
+    )
+    #title = doc.css(".item-title h3").css("a").map {|t| t.text.strip} #removes all white space and provides all names for games
+    #genre = doc.css("span.item-genre").map{|g| g.text.strip} #removes all white space and provides all genres for games
+    #release_date = doc.css("div.releaseDate.grid_3.omega").map{|r| r.text.strip}
+    #url = doc.css(".item-title h3 a").map{|u| "http://www.ign.com" + u['href']}
+    #testing = Nokogiri::HTML(open(url[20]))
+    #description = testing.css("div.gameInfo p").text.strip.gsub("\r\n", "")
+  end
+
+  def self.all
+    @@all
+  end
 
   def self.upcoming
     all.each.with_index(1) do |d, index|
       puts "#{index}. #{d.name}"
     end
+  end
+
+  def description
+    @description ||= game_description_doc.css("div.gameInfo p").text.strip.gsub("\r\n", "")
   end
 
   def self.info
@@ -31,26 +54,24 @@ class UpcomingGames::Games
     game_2.genre = "VR Horror"
     game_2.description = "Discover who you are before it’s too late in The Inpatient; a PS VR Exclusive set in the Blackwood Sanatorium, over 60 years prior to the 2016 BAFTA award-winning Until Dawn. With deep immersion that places YOU in the game, you take on the role of an amnesiac inpatient who must find out who you are and why you are in the Sanatorium. With a branching narrative, and different endings; every choice you make can drastically affect the way the game plays out. "
     @@all << game_2
-end
-
-  def self.all
-    @@all
   end
 
-  def self.new_from_page(g)
-    title = doc.css(".item-title h3").css("a").map {|t| t.text.strip} #removes all white space and provides all names for games
-    genre = doc.css("span.item-genre").map{|g| g.text.strip} #removes all white space and provides all genres for games
-    release_date = doc.css("div.releaseDate.grid_3.omega").map{|r| r.text.strip}
-    url = doc.css(".item-title h3 a").map{|u| "http://www.ign.com" + u['href']}
-    testing = Nokogiri::HTML(open(url[20]))
-    description = testing.css("div.gameInfo p").text.strip.gsub("\r\n", "")
-  end
 
-  def description_scraper
-    doc = Nokogiri::HTML(open(self.url))
-  end
+    def self.scrape_upcoming_games
+      doc = Nokogiri::HTML(open("http://www.ign.com/upcoming/games?platformSlug=ps4&sortBy=releaseDate&sortOrder=asc&time=3m"))
+      games = doc.css("div.clear.itemList-item")
+      games.each do |g|
+        UpcomingGames::Games.new_from_page(g)
+        binding.pry
+      end
+    end
 
-  def doc
-    @doc ||= Nokogiri::HTML(open("http://www.ign.com/upcoming/games?platformSlug=ps4&sortBy=releaseDate&sortOrder=asc&time=3m"))
-  end
+    def game_description_doc
+      @game_description_doc ||= Nokogiri::HTML(open("http://www.ign.com#{self.url}"))
+    end
+
+    def doc
+    @doc ||= Nokogiri::HTML(open(self))
+    end
+
 end
